1e74962707a9266674f0499a65a6abb46bbb0423
asipes <dr.crewcut@gmail.com>
2024-12-26 20:03:20 +0300

    Add ArrangingCoins solution.

---
+++ app/src/test/java/org/solutions/easy/ArrangingCoinsTest.java
@@ -0,0 +1,31 @@
+package org.solutions.easy;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrangingCoinsTest {
+
+    @ParameterizedTest
+    @MethodSource("provideArrangeCoinsTestCases")
+    void arrangeCoins(int n, int expected) {
+        ArrangingCoins arrangingCoins = new ArrangingCoins();
+        var actual = arrangingCoins.arrangeCoins(n);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    public static Stream<Arguments> provideArrangeCoinsTestCases() {
+        return Stream.of(
+                Arguments.of(5, 2),
+                Arguments.of(8, 3),
+                Arguments.of(1804289383, 60070)
+        );
+    }
+}
