bf9c4b759b46a5cd7613dc28b562b80809d02e05
asipes <dr.crewcut@gmail.com>
2025-01-15 18:14:39 +0300

    Add FibonacciNumber solution.

---
+++ app/src/main/java/org/solutions/leetcode/medium/FibonacciNumber.java
@@ -0,0 +1,26 @@
+package org.solutions.leetcode.medium;
+
+/**
+ * <a href="https://leetcode.com/problems/fibonacci-number">509. Fibonacci Number</a>
+ */
+
+public class FibonacciNumber {
+    public int fib(int n) {
+        if (n < 2) {
+            return n;
+        }
+
+        int f = 2;
+        int n1 = 0;
+        int n2 = 1;
+
+        while (f < n) {
+            int temp = n1 + n2;
+            n1 = n2;
+            n2 = temp;
+            f++;
+        }
+
+        return n1 + n2;
+    }
+}
---
+++ app/src/test/java/org/solutions/leetcode/medium/FibonacciNumberTest.java
@@ -0,0 +1,30 @@
+package org.solutions.leetcode.medium;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FibonacciNumberTest {
+
+    public static Stream<Arguments> provideFibonacciNumberTestCases() {
+        return Stream.of(
+                Arguments.of(2, 1),
+                Arguments.of(3, 2),
+                Arguments.of(4, 3),
+                Arguments.of(5, 5),
+                Arguments.of(6, 8)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideFibonacciNumberTestCases")
+    void fib(int n , int expected) {
+        var actual = new FibonacciNumber().fib(n);
+
+        assertEquals(expected, actual);
+    }
+}
