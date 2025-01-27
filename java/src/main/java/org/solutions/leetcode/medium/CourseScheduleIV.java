6fac0f8940b9f73910f1be38fc2278a9d4f51701
asipes <andrewkirpiy@yandex.ru>
2025-01-27 04:52:20 +0300

    Add CourseScheduleIV java solution.

--- java/src/main/java/org/solutions/leetcode/medium/CourseScheduleIV.java
+++ java/src/main/java/org/solutions/leetcode/medium/CourseScheduleIV.java
@@ -22,43 +22,76 @@ public class CourseScheduleIV {
 
         boolean[][] isReachable = new boolean[numCourses][numCourses];
 
-        computeTransitiveClosureTopological(numCourses, isReachable, graph);
+        for (int i = 0; i < numCourses; i++) {
+            dfs(i, i, graph, isReachable);
+        }
 
         List<Boolean> result = new ArrayList<>();
 
-        for (int i = 0; i < queries.length; i++) {
-            int[] query = queries[i];
-
+        for (int[] query : queries) {
             result.add(isReachable[query[0]][query[1]]);
         }
 
         return result;
     }
 
+    private void dfs(int start, int node, List<List<Integer>> graph, boolean[][] isReachable) {
+        for (int neighbor : graph.get(node)) {
+            if (!isReachable[start][neighbor]) {
+                isReachable[start][neighbor] = true;
+                dfs(start, neighbor, graph, isReachable);
+            }
+        }
+    }
+
+    // медленнее
+    @SuppressWarnings("unused")
+    private void computeTransitiveClosure(int numCourses, boolean[][] isReachable, List<List<Integer>> graph) {
+        for (int i = 0; i < numCourses; i++) {
+            for (int neighbor : graph.get(i)) {
+                isReachable[i][neighbor] = true;
+            }
+        }
+
+        // Что такое транзитивное замыкание через алгоритм Флойда-Уоршалла
+        // Проверяем можно ли пройти из i в j через k
+        for (int k = 0; k < numCourses; k++) {
+            for (int i = 0; i < numCourses; i++) {
+                for (int j = 0; j < numCourses; j++) {
+                    if (isReachable[i][k] && isReachable[k][j]) {
+                        isReachable[i][j] = true;
+                    }
+                }
+            }
+        }
+    }
+
+    // На некоторых кейсах не работает хз почему
+    @SuppressWarnings("unused")
     private void computeTransitiveClosureTopological(int numCourses, boolean[][] isReachable, List<List<Integer>> graph) {
-        int[] isDegree = new int[numCourses];
-        List<Integer> topological = new ArrayList<>();
+        int[] inDegree = new int[numCourses];
         Queue<Integer> queue = new LinkedList<>();
 
         for (int i = 0; i < numCourses; i++) {
             for (int neighbor : graph.get(i)) {
-                isDegree[neighbor]++;
+                inDegree[neighbor]++;
             }
         }
 
         for (int i = 0; i < numCourses; i++) {
-            if (isDegree[i] == 0) {
+            if (inDegree[i] == 0) {
                 queue.offer(i);
             }
         }
 
+        List<Integer> topological = new ArrayList<>();
         while (!queue.isEmpty()) {
             int cur = queue.poll();
             topological.add(cur);
 
             for (int neighbor : graph.get(cur)) {
-                isDegree[neighbor]--;
-                if (isDegree[neighbor] == 0) {
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0) {
                     queue.offer(neighbor);
                 }
             }
