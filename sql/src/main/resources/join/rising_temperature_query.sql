409de78d833ee57ccfe379df45c6de935120b5ae
asipes <andrewkirpiy@yandex.ru>
2025-01-28 18:29:20 +0300

    Add Rising Temperature sql solution.

--- sql/src/main/resources/join/rising_temperature_query.sql
+++ sql/src/main/resources/join/rising_temperature_query.sql
@@ -4,3 +4,9 @@ SELECT w1.id
 FROM weather w1
          JOIN weather w2 ON w1.recorddate = w2.recorddate + INTERVAL '1 day'
 WHERE w1.temperature > w2.temperature;
+
+
+-- SELECT w1.id
+-- FROM weather w1
+--          JOIN weather w2 ON w1.recorddate = DATE_ADD(w2.recorddate + INTERVAL '1 day')
+-- WHERE w1.temperature > w2.temperature;
