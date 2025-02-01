686795c48c799de7c76f62d71dc10b7d5f887b5f
asipes <andrewkirpiy@yandex.ru>
2025-02-01 09:17:59 +0300

    Add FindModeInBinarySearchTree java solution.

--- java/src/main/java/org/solutions/leetcode/easy/FindModeInBinarySearchTree.java
+++ java/src/main/java/org/solutions/leetcode/easy/FindModeInBinarySearchTree.java
@@ -2,18 +2,50 @@ package org.solutions.leetcode.easy;
 
 import org.model.TreeNode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class FindModeInBinarySearchTree {
-    Map<Integer, Integer> map = new HashMap<>();
+    private final Map<Integer, Integer> freq = new HashMap<>();
+    private int currentVal = -1;
+    private int currentFreq = 0;
+    private int maxFreq = 0;
+    private final Set<Integer> result = new HashSet<>();
+
 
     public int[] findMode(TreeNode root) {
+        inOrderTraversal(root);
+
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+
+    private void inOrderTraversal(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        inOrderTraversal(node.left);
+
+        currentFreq = node.val == currentVal ? currentFreq + 1 : 1;
+
+        if (currentFreq == maxFreq) {
+            result.add(node.val);
+        } else if (currentFreq > maxFreq) {
+            maxFreq = currentFreq;
+            result.clear();
+            result.add(node.val);
+        }
+
+        currentVal = node.val;
+
+        inOrderTraversal(node.right);
+    }
+
+    public int[] findModeV1(TreeNode root) {
         dfs(root);
 
-        int max = map.values().stream().mapToInt(n -> n).max().orElse(0);
+        int max = freq.values().stream().mapToInt(n -> n).max().orElse(0);
 
-        return map.entrySet().stream()
+        return freq.entrySet().stream()
                 .filter(entry -> entry.getValue() == max)
                 .mapToInt(Map.Entry::getKey)
                 .toArray();
@@ -25,7 +57,7 @@ public class FindModeInBinarySearchTree {
         }
 
         dfs(node.left);
-        map.put(node.val, map.getOrDefault(node.val, 0) + 1);
+        freq.put(node.val, freq.getOrDefault(node.val, 0) + 1);
         dfs(node.right);
     }
 }
