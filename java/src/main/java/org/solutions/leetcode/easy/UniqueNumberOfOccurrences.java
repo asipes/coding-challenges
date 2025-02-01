076d4d7a251ac3de3b28b0e9040d5755d08e2f28
asipes <andrewkirpiy@yandex.ru>
2025-02-01 12:41:24 +0300

    Add UniqueNumberOfOccurrences solution.

---
+++ java/src/main/java/org/solutions/leetcode/easy/UniqueNumberOfOccurrences.java
@@ -0,0 +1,74 @@
+package org.solutions.leetcode.easy;
+
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+/**
+ * <a href="https://leetcode.com/problems/unique-number-of-occurrences">1207. Unique Number of Occurrences</a>
+ */
+
+public class UniqueNumberOfOccurrences {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int num : arr) {
+            freq.merge(num, 1, Integer::sum);
+        }
+
+        return freq.size() == new HashSet<>(freq.values()).size();
+    }
+
+    public boolean uniqueOccurrencesV2(int[] arr) {
+        Map<Integer, Long> freq = Arrays.stream(arr)
+                .boxed()
+                .collect(Collectors.groupingBy(num -> num, Collectors.counting()));
+
+        return freq.size() == new HashSet<>(freq.values()).size();
+    }
+    public boolean uniqueOccurrencesV1(int[] arr) {
+
+        return Arrays.stream(arr)
+                .boxed()
+                .collect(new UniqueOccurrencesCollector());
+    }
+
+    static class UniqueOccurrencesCollector implements Collector<Integer, Map<Integer, Long>, Boolean> {
+
+        @Override
+        public Supplier<Map<Integer, Long>> supplier() {
+            return HashMap::new;
+        }
+
+        @Override
+        public BiConsumer<Map<Integer, Long>, Integer> accumulator() {
+            return (map, value) -> map.merge(value, 1L, Long::sum);
+        }
+
+        @Override
+        public BinaryOperator<Map<Integer, Long>> combiner() {
+            return (map1, map2) -> {
+                for (Map.Entry<Integer, Long> entry : map1.entrySet()) {
+                    map1.merge(entry.getKey(), entry.getValue(), Long::sum);
+                }
+                return map1;
+            };
+        }
+
+        @Override
+        public Function<Map<Integer, Long>, Boolean> finisher() {
+            return map -> {
+                Collection<Long> values = map.values();
+                return values.size() == new HashSet<>(values).size();
+            };
+        }
+
+        @Override
+        public Set<Characteristics> characteristics() {
+            return Set.of();
+        }
+    }
+}
---
+++ java/src/test/java/org/solutions/leetcode/easy/UniqueNumberOfOccurrencesTest.java
@@ -0,0 +1,27 @@
+package org.solutions.leetcode.easy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UniqueNumberOfOccurrencesTest {
+
+    public static Stream<Arguments> provideUniqueOccurrencesTestCases() {
+        return Stream.of(
+                Arguments.of(new int[]{1,2,2,1,1,3}, true),
+                Arguments.of(new int[]{1,2}, false),
+                Arguments.of(new int[]{-3,0,1,-3,1,1,1,-3,10,0}, true)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideUniqueOccurrencesTestCases")
+    void uniqueOccurrences(int[] arr, boolean expected) {
+        var actual = new UniqueNumberOfOccurrences().uniqueOccurrences(arr);
+        assertEquals(expected, actual);
+    }
+}
