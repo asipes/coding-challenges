c3b50939802e724fe87088f567113841a29b88c8
asipes <andrewkirpiy@yandex.ru>
2025-02-03 03:10:38 +0300

    Add ImmediateFoodDeliveryII sql solution.

--- sql/src/main/resources/aggregate/immediate_food_delivery_ii_query.sql
+++ sql/src/main/resources/aggregate/immediate_food_delivery_ii_query.sql
@@ -1,16 +1,8 @@
 -- https://leetcode.com/problems/immediate-food-delivery-ii 1174. Immediate Food Delivery II
 
 SELECT ROUND(
-                       COUNT(*) FILTER (WHERE order_date = customer_pref_delivery_date AND rank = 1) * 100.0 /
-                       COUNT(*) FILTER (WHERE rank = 1), 2) AS immediate_percentage
-FROM (SELECT *,
-             RANK() OVER (PARTITION BY customer_id ORDER BY order_date) AS rank
-      FROM delivery) AS first_orders
-
-/*
- SELECT ROUND(COUNT(*) FILTER (WHERE order_date = customer_pref_delivery_date) * 100.0 / COUNT(*), 2)::numeric(10,2) AS immediate_percentage
-FROM (SELECT DISTINCT ON (order_date) order_date,
-                                      customer_pref_delivery_date
+               AVG(CASE WHEN customer_pref_delivery_date = order_date THEN 1 ELSE 0 END) * 100.0, 2
+       ) AS immediate_percentage
+FROM (SELECT MIN(order_date) AS order_date, MIN(customer_pref_delivery_date) AS customer_pref_delivery_date
       FROM delivery
-      GROUP BY order_date, customer_pref_delivery_date) AS first_orders;
- */
+      GROUP BY customer_id) AS first_orders;
