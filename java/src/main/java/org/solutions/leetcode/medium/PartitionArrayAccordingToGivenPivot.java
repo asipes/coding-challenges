efba24428897f538c76f849e9e6ecd7fdd8c2d4e
asipes <andrewkirpiy@yandex.ru>
2025-03-04 01:50:31 +0300

    Add PartitionArrayAccordingToGivenPivot java solution.

--- java/src/main/java/org/solutions/leetcode/medium/PartitionArrayAccordingToGivenPivot.java
+++ java/src/main/java/org/solutions/leetcode/medium/PartitionArrayAccordingToGivenPivot.java
@@ -9,28 +9,23 @@ import java.util.List;
 
 public class PartitionArrayAccordingToGivenPivot {
     public int[] pivotArray(int[] nums, int pivot) {
-        List<Integer> lows = new ArrayList<>();
-        List<Integer> mid = new ArrayList<>();
         List<Integer> highs = new ArrayList<>();
+
+        int index = 0;
+        int pivotCount = 0;
 
         for (int j : nums) {
             if (j < pivot) {
-                lows.add(j);
+                nums[index++] = j;
             } else if (j > pivot) {
                 highs.add(j);
             } else {
-                mid.add(j);
+                pivotCount++;
             }
         }
 
-        int index = 0;
-
-        for (int num : lows) {
-            nums[index++] = num;
-        }
-
-        for (int num : mid) {
-            nums[index++] = num;
+        for (int i = 0; i < pivotCount; i++) {
+            nums[index++] = pivot;
         }
 
         for (int num : highs) {
